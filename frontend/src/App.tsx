c098e942b01c0052be869add7935981fede86ead
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-20 17:37:43 +0700

    feat: create customized theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from "react-router";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Chat from "./pages/Chat";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+
 function App() {
   return (
-    <>
-      <main>Hello World</main>
-    </>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
   );
 }
 
