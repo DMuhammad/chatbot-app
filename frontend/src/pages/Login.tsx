5a8488f00819bff4967c82f5b0e1405bbacfd396
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-20 20:39:16 +0700

    feat: design login screen

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,81 @@
+import { Box, Button, Typography } from "@mui/material";
+import { IoIosLogIn } from "react-icons/io";
+import CustomizedInput from "../components/Shared/CustomizedInput";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log(email, password);
+  };
+  return (
+    <Box width={"100%"} height={"100%"} display={"flex"} flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="airobot.png" alt="AI Robot" style={{ width: "400px" }} />
+      </Box>
+      <Box
+        display={"flex"}
+        flex={{ xs: 1, md: 0.5 }}
+        justifyContent={"center"}
+        alignItems={"center"}
+        padding={2}
+        ml={"auto"}
+        mt={16}
+      >
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10 px 10px 20px #000",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Typography
+              variant="h4"
+              textAlign={"center"}
+              padding={2}
+              fontWeight={600}
+            >
+              Login
+            </Typography>
+            <CustomizedInput label="Email" name="email" type="email" />
+            <CustomizedInput label="Password" name="password" type="password" />
+            <Button
+              type="submit"
+              sx={{
+                px: 2,
+                py: 2,
+                mt: 2,
+                width: "400px",
+                borderRadius: 2,
+                bgcolor: "#00fffc",
+                ":hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              endIcon={<IoIosLogIn />}
+            >
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default Login;
