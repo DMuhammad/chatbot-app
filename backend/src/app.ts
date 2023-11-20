0c3fc8530fc370288ac0ab60e172af2d62c66c85
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-20 13:08:35 +0700

    feat: Add logging middleware and route handling in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 import "dotenv/config";
+import routes from "./routes/index.js";
 
 const app = express();
 
 // middlewares
 app.use(express.json());
+
+// remove when in production
+app.use(morgan("dev"));
+
+app.use("/api/v1", routes);
 
 export default app;
