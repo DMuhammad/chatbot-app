6953c63b903805bfce21e42ab20e2c3206c676e4
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-20 18:40:40 +0700

    feat: design app header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+import Logo from "./Shared/Logo";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <AppBar
+      sx={{
+        bgcolor: "transparent",
+        position: "static",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { createTheme, ThemeProvider } from "@mui/material";
 
 const theme = createTheme({
   typography: {
-    fontFamily: "Roboto Slap, serif",
+    fontFamily: "Roboto Slab, serif",
     allVariants: { color: "white" },
   },
 });
