85e7c0a9e349a304d344f9a4a84abfac2d6bf41f
Dzikri Muhammad <notfnd123@gmail.com>
2023-11-21 11:02:19 +0700

    feat: design chat page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,175 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Typography,
+  colors,
+} from "@mui/material";
+import { IoMdSend } from "react-icons/io";
+import { UseAuth } from "../context/AuthContext";
+import ChatItem from "../components/Chat/ChatItem";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const auth = UseAuth();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [chatMessages, setChatMessages] = useState([]);
+  const handleSubmit = async () => {
+    console.log(inputRef.current?.value);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", sm: "none", xs: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17, 29, 39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {auth?.user?.name[0]}
+            {auth?.user?.name.split(" ")[1][0]}
+          </Avatar>
+          <Typography
+            sx={{
+              mx: "auto",
+              fontFamily: "work sans",
+            }}
+          >
+            You are talking to a ChatBOT
+          </Typography>
+          <Typography
+            sx={{
+              mx: "auto",
+              fontFamily: "work sans",
+              my: 4,
+              p: 3,
+            }}
+          >
+            You can ask some questions related to Knowledge, Business, Advices,
+            Education, etc. But avoid sharing personal information
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: "700",
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: colors.red[300],
+              ":hover": {
+                bgcolor: colors.red.A400,
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, sm: 1, xs: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            mx: "auto",
+            fontWeight: "600",
+          }}
+        >
+          Model - GPT 3.5 Turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        >
+          {chatMessages.map((chat, index) => (
+            // @ts-ignore
+            <ChatItem content={chat.content} role={chat.role} key={index} />
+          ))}
+        </Box>
+        <div
+          style={{
+            width: "100%",
+            borderRadius: 8,
+            backgroundColor: "rgb(17, 27, 39)",
+            display: "flex",
+            margin: "auto",
+          }}
+        >
+          <input
+            ref={inputRef}
+            type="text"
+            style={{
+              width: "100%",
+              backgroundColor: "transparent",
+              padding: "30px",
+              border: "none",
+              outline: "none",
+              color: "white",
+              fontSize: "20px",
+            }}
+          />
+          <IconButton
+            onClick={handleSubmit}
+            sx={{
+              color: "white",
+              mx: 1,
+            }}
+          >
+            <IoMdSend />
+          </IconButton>
+        </div>
+      </Box>
+    </Box>
+  );
 };
 
 export default Chat;
